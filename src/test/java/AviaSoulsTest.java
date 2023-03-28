d15d50f0d6c18877160fe838a3a2f3bd9a04af1e
i.shatunova <ivashatunova@gmail.com>
2023-03-28 15:24:40 +0300

    Added test shouldSortAfterSerch and rewrite test shouldComparable

--- src/test/java/AviaSoulsTest.java
+++ src/test/java/AviaSoulsTest.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 public class AviaSoulsTest {
     Ticket ticket1 = new Ticket("DFG", "RTS", 10_000, 5, 7);
     Ticket ticket2 = new Ticket("DFG", "RTS", 16_000, 5, 7);
-    Ticket ticket3 = new Ticket("DFG", "RTS", 100_000, 5, 7);
-    Ticket ticket4 = new Ticket("DFG", "RTS", 253_070, 5, 7);
+    Ticket ticket3 = new Ticket("GHK", "RTS", 100_000, 5, 7);
+    Ticket ticket4 = new Ticket("DFG", "GHK", 253_070, 5, 7);
     Ticket ticket5 = new Ticket("DFG", "RTS", 7_700, 5, 7);
-    Ticket ticket6 = new Ticket("DFG", "RTS", 10_054, 5, 7);
+    Ticket ticket6 = new Ticket("DFG", "GHK", 10_054, 5, 7);
 
     private AviaSouls fillTickets() {
         AviaSouls tickets = new AviaSouls();
@@ -22,20 +22,23 @@ public class AviaSoulsTest {
         return tickets;
     }
 
+
     @Test
     public void shouldComparable() {
-        AviaSouls ticketsManager = fillTickets();
+        Ticket[] actual = {ticket1, ticket2, ticket3, ticket4, ticket5, ticket6};
         Ticket[] expected = {ticket5, ticket1, ticket6, ticket2, ticket3, ticket4};
-        Ticket[] actual = ticketsManager.findAll();
         Arrays.sort(actual);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSortAfterSearch() {
+        AviaSouls ticketsManager = fillTickets();
+        Ticket[] expected = {ticket5, ticket1, ticket2};
+        Ticket[] actual = ticketsManager.search("DFG", "RTS");
         Assertions.assertArrayEquals(expected, actual);
 
 
     }
-
-//    @Test
-//    public void shouldComparable() {
-//
-//    }
 
 }
